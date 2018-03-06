e6f5e40ad0fa959bcf30c5cfcb3c823d22ff95ee
Brett Montague <bmontagu@gmail.com>
2018-03-05 21:44:18 -0500

    Revert "sepolicy: Isolate qcom sysfs_graphics policy for recovery"
    This reverts commit 1df5895fda88fe0190a43ee1301377a3d1fdff34.
    
    Change-Id: I737f388550ed6e6768232b037c3a08a24650ef9f

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -3,9 +3,6 @@
 # that inherit from AOSiP
 #
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/aosip/sepolicy/qcom/private
-
 BOARD_SEPOLICY_DIRS += \
     device/aosip/sepolicy/qcom/common \
     device/aosip/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
